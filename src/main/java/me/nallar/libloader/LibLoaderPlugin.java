5ce32f3b576b911505eccefc5c4a05d072aca998
Ross Allan <ross@nallar.me>
2017-05-12 18:42:50 +0100

    Log parent dependencies which aren't bundled or with url

--- src/main/java/me/nallar/libloader/LibLoaderPlugin.java
+++ src/main/java/me/nallar/libloader/LibLoaderPlugin.java
@@ -163,6 +163,8 @@ public class LibLoaderPlugin implements Plugin<Project> {
 			i++;
 		}
 		for (ArtifactVersion av : alreadyLibLoaded.values()) {
+			if (extension.log)
+				System.out.println("LibLoader noted parent dependency " + av.group + '.' + av.version);
 			put(attr, "LibLoader-group" + i, av.group);
 			put(attr, "LibLoader-name" + i, av.name);
 			put(attr, "LibLoader-classifier" + i, av.classifier);
